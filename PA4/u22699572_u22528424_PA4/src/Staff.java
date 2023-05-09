5e03440123c6e79a13f78fd33886861750c50ffc
Zane Augustyn <86051512+ZaneAugustyn@users.noreply.github.com>
2023-05-09 07:07:40 +0200

    Update Staff.java
    Staff filter search works now

--- PA4/u22699572_u22528424_PA4/src/Staff.java
+++ PA4/u22699572_u22528424_PA4/src/Staff.java
@@ -1,25 +1,17 @@
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
+import java.util.regex.PatternSyntaxException;
 
 public class prac4 extends JFrame {
-    String element;
-    String value;
-
-    public void setElement(String element) {
-        this.element = element;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
 
     public prac4() {
-        setTitle("Tabbed Menu Example");
+        setTitle("COS221Prac4");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1000, 450);
         setLocationRelativeTo(null);
@@ -69,54 +61,37 @@ public class prac4 extends JFrame {
         DefaultTableModel model = new DefaultTableModel(data, columnNames);
         JTable table = new JTable(model);
         table.setPreferredScrollableViewportSize(new Dimension(950, 100));
-
-        // Add the table to a scroll pane
         JScrollPane scrollPane = new JScrollPane(table);
-
-        // Add the scroll pane to the tab
         tab1.add(scrollPane);
-
-        JTextField textField1;
-        textField1 = new JTextField(20);
-        textField1.setPreferredSize(new Dimension(200, 20));
-
-        // Create button
-        JButton button = new JButton("SEARCH");
-
-        // Add components to the frame
-        tab1.add(new JLabel("Choose what to filter by:"));
-        JComboBox<String> comboBox = new JComboBox<>();
-        comboBox.addItem("");
-        comboBox.addItem("first_name");
-        comboBox.addItem("last_name");
-        comboBox.addItem("address");
-        comboBox.addItem("address2");
-        comboBox.addItem("district");
-        comboBox.addItem("city");
-        comboBox.addItem("postal_code");
-        comboBox.addItem("phone");
-        comboBox.addItem("store_id");
-        comboBox.addItem("active");
-        comboBox.addItem("last_update");
-
-
-        tab1.add(comboBox);
-        tab1.add(textField1);
-        button.setPreferredSize(new Dimension(100, 20));
-        tab1.add(button);
-        add(tab1);
-
-        comboBox.addActionListener(new ActionListener() {
+        table = new JTable(model);
+        final TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(model);
+        table.setRowSorter(sorter);
+        add(new JScrollPane(table), BorderLayout.CENTER);
+        JPanel panel = new JPanel(new BorderLayout());
+        JLabel label = new JLabel("Filter");
+        panel.add(label, BorderLayout.WEST);
+        final JTextField filterText = new JTextField("");
+        panel.add(filterText, BorderLayout.CENTER);
+        add(panel, BorderLayout.NORTH);
+        JButton button = new JButton("Filter");
+        button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                String selectedOption = (String) comboBox.getSelectedItem();
-                setElement(selectedOption);
+                String text = filterText.getText();
+                if(text.length() == 0) {
+                    sorter.setRowFilter(null);
+                } else {
+                    try {
+                        sorter.setRowFilter(RowFilter.regexFilter(text));
+                    } catch(PatternSyntaxException pse) {
+                        System.out.println("Bad regex pattern");
+                    }
+                }
             }
         });
-
-        button.addActionListener(e -> {
-            String text = textField1.getText();
-            setValue(text);
-        });
+        add(button, BorderLayout.SOUTH);
+        setSize(1000, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
 
         setVisible(true);
     }
@@ -129,7 +104,7 @@ public class prac4 extends JFrame {
         Connection conn = null;
         Statement stmt = null;
         ResultSet rs = null;
-        String[][] arr = new String[5][12];
+        String[][] arr = new String[2][12];
         String url = "jdbc:mysql://localhost:3306/sakila";
         String username = "root";
         String password = "Bakkies1";
@@ -164,46 +139,5 @@ public class prac4 extends JFrame {
         }
         return arr;
     }
-
-    public String[][] getSomeStaff() {
-        Connection conn = null;
-        Statement stmt = null;
-        ResultSet rs = null;
-        String[][] arr = new String[5][12];
-        String url = "jdbc:mysql://localhost:3306/sakila";
-        String username = "root";
-        String password = "Bakkies1";
-        try {
-
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(url, username, password);
-
-            stmt = conn.createStatement();
-            String SQL = "SELECT s.first_name, s.last_name, a.address, a.address2, a.district, c.city, a.postal_code, a.phone, s.store_id, s.active, s.last_update FROM staff AS s INNER JOIN address AS a ON s.address_id = a.address_id INNER JOIN city AS c ON a.city_id = c.city_id WHERE " +element+"='"+value+"'";
-            rs = stmt.executeQuery(SQL);
-            int x = 0;
-            while (rs.next()) {
-                //System.out.println(rs.getString("first_name")+rs.getString("last_name"));
-                arr[x][0] = rs.getString("s.first_name");
-                arr[x][1] = rs.getString("s.last_name");
-                arr[x][2] = rs.getString("a.address");
-                arr[x][3] = rs.getString("a.address2");
-                arr[x][4] = rs.getString("a.district");
-                arr[x][5] = rs.getString("c.city");
-                arr[x][6] = rs.getString("a.postal_code");
-                arr[x][7] = rs.getString("a.phone");
-                arr[x][8] = rs.getString("s.store_id");
-                arr[x][9] = rs.getString("s.active");
-                arr[x][10] = rs.getString("s.last_update");
-                x++;
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        } catch (ClassNotFoundException ex) {
-            ex.printStackTrace();
-        }
-        return arr;
-    }
-
 }
 
