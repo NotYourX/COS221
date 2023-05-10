af112a126d7e3030366cfb8933aa7366eccb7817
Zane Augustyn <86051512+ZaneAugustyn@users.noreply.github.com>
2023-05-10 08:01:43 +0200

    Update Films.java

--- PA4/u22699572_u22528424_PA4/src/Films.java
+++ PA4/u22699572_u22528424_PA4/src/Films.java
@@ -1,104 +1,4 @@
-import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
-import javax.swing.table.TableRowSorter;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.Year;
-import java.time.format.DateTimeFormatter;
-import java.util.regex.PatternSyntaxException;
-
-public class prac4 extends JFrame {
-
-   /* public prac4() {
-        setTitle("COS221Prac4");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(1000, 450);
-        setLocationRelativeTo(null);
-
-        // Create a menu bar
-        JMenuBar menuBar = new JMenuBar();
-        setJMenuBar(menuBar);
-
-        // Create a menu with three items
-        JMenu menu = new JMenu("Tabs");
-        menuBar.add(menu);
-
-        JMenuItem tab1Item = new JMenuItem("Staff");
-        JMenuItem tab2Item = new JMenuItem("Films");
-        JMenuItem tab3Item = new JMenuItem("Reports");
-        JMenuItem tab4Item = new JMenuItem("Notifications");
-
-        // Add the items to the menu
-        menu.add(tab1Item);
-        menu.add(tab2Item);
-        menu.add(tab3Item);
-        menu.add(tab4Item);
-
-        // Create a tabbed pane with three tabs
-        JTabbedPane tabbedPane = new JTabbedPane();
-        getContentPane().add(tabbedPane);
-
-        JPanel tab1 = new JPanel();
-        JPanel tab2 = new JPanel();
-        JPanel tab3 = new JPanel();
-        JPanel tab4 = new JPanel();
-
-        // Add the tabs to the tabbed pane
-        tabbedPane.addTab("Staff", tab1);
-        tabbedPane.addTab("Films", tab2);
-        tabbedPane.addTab("Reports", tab3);
-        tabbedPane.addTab("Notifications", tab4);
-
-        // Add action listeners to the menu items
-        tab1Item.addActionListener(e -> tabbedPane.setSelectedIndex(0));
-        tab2Item.addActionListener(e -> tabbedPane.setSelectedIndex(1));
-        tab3Item.addActionListener(e -> tabbedPane.setSelectedIndex(2));
-        tab4Item.addActionListener(e -> tabbedPane.setSelectedIndex(3));
-
-
-        //----------------------------------------------------TAB 1 (Staff)----------------------------------------------------------------------------------------------------
-        String[] columnNames = {"first_name", "last_name", "address", "address2", "district", "city", "postal_code", "phone", "store_id", "active", "last_update"};
-        Object[][] data = getAllStaff();
-        DefaultTableModel model = new DefaultTableModel(data, columnNames);
-        JTable table = new JTable(model);
-        table.setPreferredScrollableViewportSize(new Dimension(950, 100));
-        JScrollPane scrollPane = new JScrollPane(table);
-        tab1.add(scrollPane);
-        table = new JTable(model);
-        final TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(model);
-        table.setRowSorter(sorter);
-        add(new JScrollPane(table), BorderLayout.CENTER);
-        JPanel panel = new JPanel(new BorderLayout());
-        JLabel label = new JLabel("Filter");
-        panel.add(label, BorderLayout.WEST);
-        final JTextField filterText = new JTextField("");
-        panel.add(filterText, BorderLayout.CENTER);
-        add(panel, BorderLayout.NORTH);
-        JButton button = new JButton("Filter");
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                String text = filterText.getText();
-                if(text.length() == 0) {
-                    sorter.setRowFilter(null);
-                } else {
-                    try {
-                        sorter.setRowFilter(RowFilter.regexFilter(text));
-                    } catch(PatternSyntaxException pse) {
-                        System.out.println("Bad regex pattern");
-                    }
-                }
-            }
-        });
-        add(button, BorderLayout.SOUTH);
-    }*/
-
-    public prac4() {
+public prac4() {
 
         setTitle("FILMS");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -128,7 +28,6 @@ public class prac4 extends JFrame {
         JButton button = new JButton("Add Record");
         button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-
                 //------------------------------------------------------------------------------------------------------------------------
                     JDialog insertPopup = new JDialog();
                     JPanel insertPanel = new JPanel(new GridLayout(0, 2));
@@ -190,18 +89,24 @@ public class prac4 extends JFrame {
                             String original_language = (String) comboBox2.getSelectedItem();//**************************
                             String[] lang = new String[getNumOfLanguages()];
                             lang = getLanguages();
-                            String languageNum = "";
-                            String original_languageNum = "";
-                            for (int i = 0; i < getNumOfLanguages(); i++) {
-                                if(language == lang[i])
+                            int languageNum = 1;
+                            int original_languageNum = 1;
+                            System.out.println(language);
+                            System.out.println(original_language);
+                            for (int i = 1; i <= getNumOfLanguages(); i++) {
+                                if(language.equals(lang[i]))
                                 {
-                                    languageNum = String.valueOf(i);
+                                    System.out.println("i am in here");
+                                    //languageNum = String.valueOf(i);
+                                    int temp = i;
+                                    languageNum = temp;
                                 }
                             }
-                            for (int i = 0; i < getNumOfLanguages(); i++) {
-                                if(original_language == lang[i])
+                            for (int i = 1; i <= getNumOfLanguages(); i++) {
+                                if(original_language.equals(lang[i]))
                                 {
-                                    original_languageNum = String.valueOf(i);
+                                    int temp = i;
+                                    original_languageNum = temp;
                                 }
                             }
 
@@ -230,8 +135,8 @@ public class prac4 extends JFrame {
                                 stmt.setString(1, title);
                                 stmt.setInt(2,  year);
                                 stmt.setString(3, description);
-                                stmt.setString(4, languageNum);
-                                stmt.setString(5, original_languageNum);
+                                stmt.setInt(4, languageNum);
+                                stmt.setInt(5, original_languageNum);
                                 stmt.setInt(6, rental_duration);
                                 stmt.setFloat(7, rental_rate);
                                 stmt.setInt(8, length);
@@ -239,6 +144,9 @@ public class prac4 extends JFrame {
                                 stmt.setString(10, rating);
                                 stmt.setString(11, special_feature);
                                 stmt.executeUpdate();
+                                Object[][] filmdata = getAllFilms();
+                                DefaultTableModel dtm = (DefaultTableModel) table.getModel();
+                                dtm.setDataVector(filmdata,columnNames);
                                 conn.close();
                             } catch (SQLException ex) {
                                 ex.printStackTrace();
@@ -246,7 +154,6 @@ public class prac4 extends JFrame {
 
                             // Close the popup
                             insertPopup.dispose();
-
 
                         }
                     });
@@ -261,50 +168,7 @@ public class prac4 extends JFrame {
         });
         add(button, BorderLayout.SOUTH);
     }
-    int numOfLanguages = 0;
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new prac4().setVisible(true));
-    }
 
-    public String[][] getAllStaff() {
-        Connection conn = null;
-        Statement stmt = null;
-        ResultSet rs = null;
-        String[][] arr = new String[2][12];
-        String url = "jdbc:mysql://localhost:3306/sakila";
-        String username = "root";
-        String password = "Bakkies1";
-        try {
-
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(url, username, password);
-
-            stmt = conn.createStatement();
-            String SQL = "SELECT s.first_name, s.last_name, a.address, a.address2, a.district, c.city, a.postal_code, a.phone, s.store_id, s.active, s.last_update FROM staff AS s INNER JOIN address AS a ON s.address_id = a.address_id INNER JOIN city AS c ON a.city_id = c.city_id";
-            rs = stmt.executeQuery(SQL);
-            int x = 0;
-            while (rs.next()) {
-                //System.out.println(rs.getString("first_name")+rs.getString("last_name"));
-                arr[x][0] = rs.getString("s.first_name");
-                arr[x][1] = rs.getString("s.last_name");
-                arr[x][2] = rs.getString("a.address");
-                arr[x][3] = rs.getString("a.address2");
-                arr[x][4] = rs.getString("a.district");
-                arr[x][5] = rs.getString("c.city");
-                arr[x][6] = rs.getString("a.postal_code");
-                arr[x][7] = rs.getString("a.phone");
-                arr[x][8] = rs.getString("s.store_id");
-                arr[x][9] = rs.getString("s.active");
-                arr[x][10] = rs.getString("s.last_update");
-                x++;
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        } catch (ClassNotFoundException ex) {
-            ex.printStackTrace();
-        }
-        return arr;
-    }
     public String[][] getAllFilms() {
         Connection conn = null;
         Statement stmt = null;
@@ -338,6 +202,7 @@ public class prac4 extends JFrame {
                 arr[x][11] = rs.getString("special_features");
                 x++;
             }
+            conn.close();
         } catch (SQLException ex) {
             ex.printStackTrace();
         } catch (ClassNotFoundException ex) {
@@ -368,6 +233,7 @@ public class prac4 extends JFrame {
                 arr[x] = rs.getString("name");
                 x++;
             }
+            conn.close();
         } catch (SQLException ex) {
             ex.printStackTrace();
         } catch (ClassNotFoundException ex) {
@@ -375,12 +241,6 @@ public class prac4 extends JFrame {
         }
         return arr;
     }
-
-    public void setLanguage(int num)
-    {
-        numOfLanguages = num;
-    }
-
     public int getNumOfLanguages()
     {
         int result = 0;
@@ -414,4 +274,3 @@ public class prac4 extends JFrame {
     }
 
 }
-
